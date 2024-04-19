de8584ca16450bd8855d845d4e785e228b7f673a
AtharvVerma-290 <atharv.verma29k@gmail.com>
2024-04-19 18:41:18 +0530

    accepting file from website

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
   return render_template('homePage.html')
+
+@app.route('/process', methods = ['POST', 'GET'])
+def fileRecognizer():
+  raw_file = request.files['file']
+  if raw_file.filename != '':
+    if raw_file.filename.lower().endswith('.wav'):
+      return "File is a valid .wav file."
+    else:
+      return "File is invalid."
+  else:
+    return "No file uploaded."
+  return render_template('process.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
