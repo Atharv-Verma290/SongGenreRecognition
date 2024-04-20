bb27453f7fa7ac808d5e1991f51cf1f7a5842cd1
arjun-s-15 <iamarjun9003@gmail.com>
2024-04-21 00:31:39 +0530

    process page and upload setup added

---
+++ static/index.js
@@ -0,0 +1,19 @@
+const dropArea = document.getElementById("drop-area");
+const inputFile = document.getElementById("input-file");
+
+
+var input = document.getElementById( 'input-file' );
+var infoArea = document.getElementById( 'file-upload-filename' );
+
+input.addEventListener( 'change', showFileName );
+
+function showFileName( event ) {
+  
+  // the change event gives us the input it occurred in 
+  var input = event.srcElement;
+  
+  // the input has an array of files in the `files` property, each one has a name that you can use. We're just using the name here.
+  var fileName = input.files[0].name;
+  
+}
+
