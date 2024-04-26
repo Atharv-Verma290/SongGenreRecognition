ba1258038da58f4c4f3d3b46a72ae84c8b4aeadd
arjun-s-15 <iamarjun9003@gmail.com>
2024-04-26 23:25:28 +0530

    added display name functionality

--- static/index.js
+++ static/index.js
@@ -15,5 +15,7 @@ function showFileName( event ) {
   // the input has an array of files in the `files` property, each one has a name that you can use. We're just using the name here.
   var fileName = input.files[0].name;
   
+  // use fileName however fits your app best, i.e. add it into a div
+  infoArea.textContent = 'File name: ' + fileName;
 }
 
